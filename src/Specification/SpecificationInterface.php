0d55ba9c204be4ce473863f15809fed44843ae17
Robert Norman <robert.norman@housesimple.com>
2020-09-11 09:44:36 +0100

    Do some things to make psalm actually check things.  Also add coverage-breaking and psalm-erroring stuff

--- src/Specification/SpecificationInterface.php
+++ src/Specification/SpecificationInterface.php
@@ -4,10 +4,5 @@ namespace StrikeLabs\Specification;
 
 interface SpecificationInterface
 {
-    /**
-     * @param mixed $object
-     *
-     * @return bool
-     */
     public function isSatisfiedBy($object): bool;
 }
